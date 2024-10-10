340a89cb926ee243b74f458b4d2dd2eddaacde58
Y-Noor <noor_yadallee@hotmail.com>
2024-10-11 01:49:59 +0400

    added video functionality. must add response to ui

--- main.go
+++ main.go
@@ -49,83 +49,217 @@ func main() {
 		// Retrieve the file and prompt from form data
 		prompt := r.FormValue("prmpt")
 		fmt.Println("prompt: ", prompt)
-		file, fileHeader, err := r.FormFile("image")
+		flag := r.FormValue("flag")
 
-		if err != nil {
-			http.Error(w, "Invalid file", http.StatusBadRequest)
-			fmt.Print(fileHeader)
-			return
-		}
+		fmt.Println("flag::::::::::::::::", flag)
 
-		defer file.Close()
+		if flag == "img" {
+			file, fileHeader, err := r.FormFile("image")
 
-		// Create a new file in the uploads directory
-		ImageNewFilePath := filepath.Join(uploadPath, dtf+".jpg")
-		PromptNewFilePath := filepath.Join(uploadPath, dtf+".txt")
+			if err != nil {
+				http.Error(w, "Invalid file", http.StatusBadRequest)
+				fmt.Print(fileHeader)
+				return
+			}
 
-		newFile, err := os.Create(ImageNewFilePath)
-		if err != nil {
-			http.Error(w, "Unable to create file", http.StatusInternalServerError)
-			return
-		}
-		x, e := os.Create(PromptNewFilePath)
-		_, err = x.WriteString(prompt)
+			defer file.Close()
 
-		if e != nil {
-			log.Print("err")
-		}
+			// Create a new file in the uploads directory
+			ImageNewFilePath := filepath.Join(uploadPath, dtf+".jpg")
+			PromptNewFilePath := filepath.Join(uploadPath, dtf+".txt")
 
-		defer newFile.Close()
+			newFile, err := os.Create(ImageNewFilePath)
+			if err != nil {
+				http.Error(w, "Unable to create file", http.StatusInternalServerError)
+				return
+			}
+			x, e := os.Create(PromptNewFilePath)
+			_, err = x.WriteString(prompt)
 
-		// Copy the uploaded file to the new file on disk
-		if _, err := io.Copy(newFile, file); err != nil {
-			http.Error(w, "Unable to save file", http.StatusInternalServerError)
-			return
-		}
+			if e != nil {
+				log.Print("err")
+			}
 
-		//
-		//
+			defer newFile.Close()
 
-		ctx := context.Background()
-		// Access your API key as an environment variable (see "Set up your API key" above)
-		client, err := genai.NewClient(ctx, option.WithAPIKey(string(API_KEY)))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer client.Close()
+			// Copy the uploaded file to the new file on disk
+			if _, err := io.Copy(newFile, file); err != nil {
+				http.Error(w, "Unable to save file", http.StatusInternalServerError)
+				return
+			}
 
-		modelfile, err := client.UploadFileFromPath(ctx, filepath.Join(uploadPath, dtf+".jpg"), nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer client.DeleteFile(ctx, modelfile.Name)
+			//
+			//
 
-		model := client.GenerativeModel("gemini-1.5-flash")
-		resp, err := model.GenerateContent(ctx,
-			genai.FileData{URI: modelfile.URI},
-			genai.Text(prompt))
-		if err != nil {
-			log.Fatal(err)
-		}
+			ctx := context.Background()
+			// Access your API key as an environment variable (see "Set up your API key" above)
+			client, err := genai.NewClient(ctx, option.WithAPIKey(string(API_KEY)))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer client.Close()
 
-		// printResponse(resp)
-		// model := client.GenerativeModel("gemini-1.5-flash")
-		// resp, err := model.GenerateContent(ctx, genai.Text(prompt))
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
+			modelfile, err := client.UploadFileFromPath(ctx, filepath.Join(uploadPath, dtf+".jpg"), nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer client.DeleteFile(ctx, modelfile.Name)
 
-		response := ""
-		if resp.Candidates != nil {
-			for _, v := range resp.Candidates {
-				for _, k := range v.Content.Parts {
-					response = response + string(k.(genai.Text))
+			// ----------------------------- THIS ONE
+			model := client.GenerativeModel("gemini-1.5-flash")
+			resp, err := model.GenerateContent(ctx,
+				genai.FileData{URI: modelfile.URI},
+				genai.Text(prompt))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// printResponse(resp)
+			// model := client.GenerativeModel("gemini-1.5-flash")
+			// resp, err := model.GenerateContent(ctx, genai.Text(prompt))
+			// if err != nil {
+			// 	log.Fatal(err)
+			// }
+
+			response := ""
+			if resp.Candidates != nil {
+				for _, v := range resp.Candidates {
+					for _, k := range v.Content.Parts {
+						response = response + string(k.(genai.Text))
+					}
 				}
 			}
-		}
 
-		fmt.Fprintf(w, response)
-		// fmt.Fprintf(w, "Successfully uploaded: %s\n", fileHeader.Filename)
+			// fmt.Fprintf(w, response)
+			// fmt.Fprintf(w, "Successfully uploaded: %s\n", fileHeader.Filename)
+			templ, _ := template.New("t").Parse(response)
+			templ.Execute(w, nil)
+		} else if flag == "vid" {
+			fmt.Print("videoooooooooo")
+
+			file, fileHeader, err := r.FormFile("video")
+
+			if err != nil {
+				http.Error(w, "Invalid file", http.StatusBadRequest)
+				fmt.Print(fileHeader)
+				return
+			}
+
+			defer file.Close()
+
+			ImageNewFilePath := filepath.Join(uploadPath, dtf+".mp4")
+			PromptNewFilePath := filepath.Join(uploadPath, dtf+".txt")
+
+			newFile, err := os.Create(ImageNewFilePath)
+			if err != nil {
+				http.Error(w, "Unable to create file", http.StatusInternalServerError)
+				return
+			}
+			x, e := os.Create(PromptNewFilePath)
+			_, err = x.WriteString(prompt)
+
+			if e != nil {
+				log.Print("err")
+			}
+
+			defer newFile.Close()
+
+			// Copy the uploaded file to the new file on disk
+			if _, err := io.Copy(newFile, file); err != nil {
+				http.Error(w, "Unable to save file", http.StatusInternalServerError)
+				return
+			}
+
+			ctx := context.Background()
+			// Access your API key as an environment variable
+			client, err := genai.NewClient(ctx, option.WithAPIKey(string(API_KEY)))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer client.Close()
+
+			// Use client.UploadFile to upload a file to the service.
+			// Pass it an io.Reader.
+			f, err := os.Open(ImageNewFilePath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer f.Close()
+
+			// Optionally set a display name.
+			opts := genai.UploadFileOptions{DisplayName: "dtf"}
+			// Let the API generate a unique `name` for the file by passing an empty string.
+			// If you specify a `name`, then it has to be globally unique.
+			response, err := client.UploadFile(ctx, "", f, &opts)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// View the response.
+			var VideoFile *genai.File = response
+			fmt.Printf("Uploaded file %s as: %q\n", VideoFile.DisplayName, VideoFile.URI)
+
+			// Poll GetFile() on a set interval (10 seconds here) to
+			// check file state.
+			for response.State == genai.FileStateProcessing {
+				fmt.Print(".")
+				// Sleep for 10 seconds
+				time.Sleep(10 * time.Second)
+
+				// Fetch the file from the API again.
+				response, err = client.GetFile(ctx, VideoFile.Name)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+			fmt.Println()
+
+			// View the response.
+			fmt.Printf("File %s is ready for inference as: %q\n",
+				response.DisplayName, response.URI)
+
+			prompt := []genai.Part{
+				genai.FileData{URI: response.URI},
+				genai.Text("Summarize this video. Then create a quiz with " +
+					" answer key based on the information in the video."),
+			}
+			model := client.GenerativeModel("gemini-1.5-flash")
+			// Generate content using the prompt.
+			resp, err := model.GenerateContent(ctx, prompt...)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Handle the response of generated text.
+			for _, c := range resp.Candidates {
+				if c.Content != nil {
+					fmt.Println(*c.Content)
+				}
+			}
+
+			vPrompt := []genai.Part{
+				genai.FileData{URI: response.URI},
+				genai.Text("Summarize this video. Then create a quiz with " +
+					" answer key based on the information in the video."),
+			}
+
+			// Generate content using the prompt.
+			vResp, err := model.GenerateContent(ctx, vPrompt...)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Handle the response of generated text.
+			for _, c := range vResp.Candidates {
+				if c.Content != nil {
+					fmt.Println(*c.Content)
+				}
+			}
+
+			// response := "videoooooo"
+			// templ, _ := template.New("t").Parse(response)
+			// templ.Execute(w, nil)
+		}
 	}
 
 	http.HandleFunc("/", h1)
