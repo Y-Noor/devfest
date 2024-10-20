1b28ec0384aeea3f5daf981c552c8107438e2341
noor yadallee <59338864+Y-Noor@users.noreply.github.com>
2024-10-21 02:38:18 +0400

    Delete static directory

--- static/script.js
+++
@@ -1,9 +0,0 @@
-const videoElement = document.getElementById("videoElement");
-
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-        videoElement.srcObject = stream;
-    })
-    .catch(error => {
-        console.error("Error accessing camera:", error);
-    });
